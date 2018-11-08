c5539a1f5b36448e40cb323b8bacddf70abb9897
王明亮 <wangmingliang@wangmingliangdemacbook-pro.local>
2018-11-08 10:42:59 +0800

    Initial Commit

---
+++ LXSearchBar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LXSearchBar
+//
+//  Created by 王明亮 on 2018/11/8.
+//  Copyright © 2018 王明亮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LXSearchBarTests/LXSearchBarTests.m
@@ -0,0 +1,37 @@
+//
+//  LXSearchBarTests.m
+//  LXSearchBarTests
+//
+//  Created by 王明亮 on 2018/11/8.
+//  Copyright © 2018 王明亮. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface LXSearchBarTests : XCTestCase
+
+@end
+
+@implementation LXSearchBarTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
